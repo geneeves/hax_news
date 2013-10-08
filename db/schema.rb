5e11a5665db89cb4a3b51bc1d65e498f0e7904c7
Geneva Skeen <geneva.skeen@gmail.com>
2013-10-08 10:15:58 -0700

    Completes login and logout function for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131007190718) do
 
   # These are extensions that must be enabled in order to support this database
@@ -32,7 +31,6 @@ ActiveRecord::Schema.define(version: 20131007190718) do
     t.datetime "updated_at"
   end
 
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
@@ -40,7 +38,6 @@ ActiveRecord::Schema.define(version: 20131007190718) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "votes", force: true do |t|
     t.integer  "link_id"
