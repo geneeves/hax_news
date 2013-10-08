48ac4045547025f3de0db6d98ed915511d2f98e4
Geneva Skeen <geneva.skeen@gmail.com>
2013-10-08 10:05:31 -0700

    Revises logout function for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ HackerNews::Application.routes.draw do
   resources :votes, :only => [:create]
   resources :comments, :only => [:new, :create, :destroy]
   resources :users, :only => [:new, :create]
-  resources :sessions
+  resource :sessions, :only => [:new, :create, :destroy]
 
 
   root :to => 'links#index'
