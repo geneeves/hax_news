70150c10d339b8e4f8e00be9c0188f9a382c4081
geneeves <geneeves@users.noreply.github.com>
2013-11-07 11:59:53 -0800

    reorganize attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-
-  has_secure_password
-
   validates_presence_of :name
   validates_presence_of :email
+  has_secure_password
   has_many :votes
   has_many :comments
   has_many :links
